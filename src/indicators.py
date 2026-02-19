ebf888a950cc6bfa1342e5d4ef4834ef16cd6d24
EmreSto <yusufemreyllmz@gmail.com>
2026-02-19 18:21:24 +0300

    Changed moving avg calculations to more responsive version

--- src/indicators.py
+++ src/indicators.py
@@ -1,23 +1,29 @@
 import numpy as np
 import pandas as pd 
+def hma(series, length):
+    half_wma = series.rolling(length // 2).apply(lambda x: np.average(x, weights=range(1, len(x)+1)))
+    full_wma = series.rolling(length).apply(lambda x: np.average(x, weights=range(1, len(x)+1)))
+    sqrt_len = round(np.sqrt(length))
+    diff = 2 * half_wma - full_wma
+    return diff.rolling(sqrt_len).apply(lambda x: np.average(x, weights=range(1, len(x)+1)))   
 def ssl_channels(df,length):
     high = df['High']
     low = df['Low']
     close = df['Close']
-    SMA_high = high.rolling(length).mean()
-    SMA_low = low.rolling(length).mean()
+    hma_high = hma(high, length)
+    hma_low = hma(low, length)
     hlv = np.zeros(len(df))
     for i in range(length, len(df)):
-        if close[i] >SMA_high[i]:
+        if close[i] >hma_high[i]:
             hlv[i] = 1
-        elif close[i] < SMA_low[i]:
+        elif close[i] < hma_low[i]:
             hlv[i] = -1
         else:
             hlv[i] = hlv[i-1]    
-    return hlv, SMA_high, SMA_low
+    return hlv, hma_high, hma_low
 def crossoverdetection(df):
-    ssl60, SMA_high60, SMA_low60 = ssl_channels(df,60)
-    ssl120, SMA_high120, SMA_low120 = ssl_channels(df,120)  
+    ssl60, hma_high60, hma_low60 = ssl_channels(df,60)
+    ssl120, hma_high120, hma_low120 = ssl_channels(df,120)  
     crossover = np.zeros(len(df))
     for i in range(1, len(df)):
         if ssl60[i] == 1 and ssl120[i] == 1 and (ssl60[i-1] != 1 or ssl120[i-1] != 1):
@@ -45,8 +51,8 @@ def alpha_trend(df, length=14, alpha=1.0):
             positive_flow[i] = raw_money_flow[i]
         elif typical_price[i] < typical_price[i-1]:
             negative_flow[i] = raw_money_flow[i]
-    positive_sum = pd.Series(positive_flow).rolling(14).sum()
-    negative_sum = pd.Series(negative_flow).rolling(14).sum()
+    positive_sum = pd.Series(positive_flow).rolling(length).sum()
+    negative_sum = pd.Series(negative_flow).rolling(length).sum()
     negative_sum = negative_sum.replace(0, np.nan)
     money_flow_index = 100 - (100 / (1 + (positive_sum / negative_sum)))
     at = np.zeros(len(df))
@@ -57,7 +63,8 @@ def alpha_trend(df, length=14, alpha=1.0):
             at[i] = min(downT[i], at[i-1])
     return at
 def EMA200(df):
-    ema200 = df['Close'].ewm(span=200, adjust=False).mean()
-    cloud_upper = ema200 * 1.002
-    cloud_lower = ema200 * 0.998
-    return ema200 , cloud_upper, cloud_lower
+    ema_close = df['Close'].ewm(span=200, adjust=False).mean()
+    ema_high  = df['High'].ewm(span=200, adjust=False).mean()
+    cloud_upper = ema_high
+    cloud_lower = ema_close
+    return ema_close, cloud_upper, cloud_lower
